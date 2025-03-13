33a7537756e82dd23c2bb0b57df752cae8273975
Khang <ankhangn@visglobalsolutions.com>
2025-03-13 10:43:26 +0700

    update

--- config/middleware.js
+++ config/middleware.js
@@ -1,8 +1,19 @@
-module.exports = {
-  settings: {
-    cors: {
-      enabled: true,
-      origin: ['https://sinar-urban-facilities-demo.kane-service.shop']
+module.exports = [
+    'strapi::errors',
+    {
+      name: 'strapi::cors',
+      config: {
+        origin: ['*'],  // Update this with your frontend domain
+        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+        headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+        keepHeaderOnError: true,
+      },
     },
-  },
-}; 
+    'strapi::poweredBy',
+    'strapi::logger',
+    'strapi::query',
+    'strapi::body',
+    'strapi::session',
+    'strapi::favicon',
+    'strapi::public',
+  ]; 
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,11 +3,19 @@ module.exports = {
     {
       name: 'strapi',
       cwd: '/home/u595571863/domains/kane-service.shop/public_html/sinar-urban-facilities-demo',
-      script: 'npm',
+      script: './node_modules/@strapi/strapi/bin/strapi.js',
       args: 'start',
       env: {
         NODE_ENV: 'production',
-        PORT: 1337
+        PORT: 5000,
+        HOST: '0.0.0.0',
+        DATABASE_CLIENT: 'mysql',
+        DATABASE_HOST: '127.0.0.1',
+        DATABASE_PORT: '3306',
+        DATABASE_NAME: 'u595571863_suf_demo',
+        DATABASE_USERNAME: 'u595571863_suf_demo',
+        DATABASE_PASSWORD: 'pk9*_cJLPt@3ShC',
+        DATABASE_SSL: 'false'
       },
       max_memory_restart: '1G',
       max_restarts: 10,
