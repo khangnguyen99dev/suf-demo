674519b24cd39cde8d1d2452073d90a19093637e
Khang <ankhangn@visglobalsolutions.com>
2025-03-13 10:20:23 +0700

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,6 +7,9 @@ module.exports = {
       env: {
         NODE_ENV: 'production',
       },
+      max_memory_restart: '1G',
+      max_restarts: 10,
+      node_args: '--max-old-space-size=1536'
     },
   ],
 }; 
--- frontend/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: true,
-    api_url: 'https://api.sinarurbanfacilities.com/api',
-    site_url: 'https://api.sinarurbanfacilities.com',
-    api_token_identifier: 'APIToken@sinarurbanfacilities'
-};
