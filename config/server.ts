5584a11eed8b9efe4816d3d1f6e7a46c2b271699
Khang <ankhangn@visglobalsolutions.com>
2025-03-13 14:12:18 +0700

    fix

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'https://sinar-urban-facilities-demo.kane-service.shop'),
+  url: env('PUBLIC_URL', 'https://sinar-urban-facilities-demo.onrender.com'),
   app: {
     keys: env.array('APP_KEYS'),
   },
