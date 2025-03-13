60ecdb825c4303b3fc960906b53daac1ac18dc74
Khang <ankhangn@visglobalsolutions.com>
2025-03-13 10:36:35 +0700

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,10 +2,12 @@ module.exports = {
   apps: [
     {
       name: 'strapi',
+      cwd: '/home/u595571863/domains/kane-service.shop/public_html/sinar-urban-facilities-demo',
       script: 'npm',
       args: 'start',
       env: {
         NODE_ENV: 'production',
+        PORT: 1337
       },
       max_memory_restart: '1G',
       max_restarts: 10,
